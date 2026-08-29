01a3a9e5bc52821b4d6573ba536877cfa905021b
5UP3R TH30 B4G <68150571+5UP3RTH30B4G@users.noreply.github.com>
2026-08-29 23:36:49 +0200

    Add door part definitions and state handling to NewdoorPartBlock
    Fixed #16

--- src/main/java/net/theo/scpalarm/block/NewdoorPartBlock.java
+++ src/main/java/net/theo/scpalarm/block/NewdoorPartBlock.java
@@ -7,6 +7,9 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
@@ -14,11 +17,41 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 public class NewdoorPartBlock extends Block {
+	public enum DoorPart implements net.minecraft.util.StringRepresentable {
+		LEFT_DOWN("left_down"),
+		LEFT_UP("left_up"),
+		RIGHT_UP("right_up"),
+		RIGHT_MID("right_mid"),
+		RIGHT_DOWN("right_down");
+
+		private final String name;
+
+		DoorPart(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String getSerializedName() {
+			return this.name;
+		}
+	}
+
+	public static final EnumProperty<DoorPart> DOOR_PART = EnumProperty.create("door_part", DoorPart.class);
+	public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
+
 	public NewdoorPartBlock() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.EMPTY).strength(1f, 10f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+		this.registerDefaultState(this.stateDefinition.any().setValue(DOOR_PART, DoorPart.LEFT_DOWN).setValue(FACING, Direction.NORTH));
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(DOOR_PART, FACING);
 	}
 
 	@Override
@@ -38,7 +71,11 @@ public class NewdoorPartBlock extends Block {
 
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return box(0, 0, 4, 16, 16, 12);
+		return switch (state.getValue(FACING)) {
+			case NORTH, SOUTH -> box(0, 0, 4, 16, 16, 12);
+			case EAST, WEST -> box(4, 0, 0, 12, 16, 16);
+			default -> box(0, 0, 4, 16, 16, 12);
+		};
 	}
 
 	@Override
